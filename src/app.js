1db2e4e57b11f74ddcb7bd7dc102835049b914e4
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2025-09-25 21:28:50 +0000

    initiliazed project

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+//Basic route
+app.get("/", (req, res) => {
+  res.send("Welcome to ShelfLife API!");
+});
+
+//Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
